7527160c4e0d1f5f1c328dffafbed87f6c0fe6bb
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-07-26 07:04:05 +0100

    setting up footer and nav

---
+++ sticky-app/src/pages/components/footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import '../styles/globals.css'
+
+export default function Nav(){
+    return(
+        <div className='footer'>
+            <footer>© 2023 Cerulean Lion Sticky Note App</footer>
+        </div>
+    )
+}
---
+++ sticky-app/src/pages/components/nav.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import '../styles/globals.css'
+
+export default function Nav(){
+    return(
+        <div className='nav'>
+            <nav>Cerulean Lion Sticky Note App</nav>
+        </div>
+    )
+}
