08b12cec30d59a998e4ca13455542fecb5500381
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-07-30 00:35:27 +0100

    .

--- sticky-app/src/pages/components/nav.js
+++ sticky-app/src/pages/components/nav.js
@@ -5,6 +5,7 @@ export default function Nav(){
     return(
         <div className='nav'>
             <nav>Cerulean Lion Sticky Note App</nav>
+            <img src=''></img>
         </div>
     )
 }
