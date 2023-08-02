43f995260d2bee3dff37b36c3d476f90cb61a1c4
Dedios <dediosprogramming@gmail.com>
2023-08-02 16:52:51 -0400

    creating updateNote logic

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -5,65 +5,81 @@ const router = express.Router();
 
 // Get all notes
 export const getNotes = router.get("/notes", (req, res) => {
-	Note.find({})
-		.then((notes) => {
-			res.status(200).json(notes);
-		})
-		.catch((err) => {
-			console.error("Error getting notes:", err);
-			return res.status(500).json({ message: "Internal Server Error" });
-		});
+  Note.find({})
+    .then((notes) => {
+      res.status(200).json(notes);
+    })
+    .catch((err) => {
+      console.error("Error getting notes:", err);
+      return res.status(500).json({ message: "Internal Server Error" });
+    });
 });
 
 // Get a note by ID
 export const getNoteById = router.get("/notes/:id", (req, res) => {
-	const id = req.params.id;
+  const id = req.params.id;
 
-	Note.findById(id)
-		.then((note) => {
-			if (!note) {
-				return res.status(404).json({ message: "Note not found" });
-			}
-			res.status(200).json(note);
-		})
-		.catch((err) => {
-			console.error("Error getting notes:", err);
-			return res.status(500).json({ message: "Internal Server Error" });
-		});
+  Note.findById(id)
+    .then((note) => {
+      if (!note) {
+        return res.status(404).json({ message: "Note not found" });
+      }
+      res.status(200).json(note);
+    })
+    .catch((err) => {
+      console.error("Error getting notes:", err);
+      return res.status(500).json({ message: "Internal Server Error" });
+    });
 });
 
 // Create a new note
 export const createNote = router.post("/notes", (req, res) => {
-	const { title, content } = req.body;
-	const note = new Note({ title, content });
+  const { title, content } = req.body;
+  const note = new Note({ title, content });
 
-	note.save({})
-		.then((result) => {
-			res.status(201).json(result);
-		})
-		.catch((err) => {
-			console.error("Error creating note:", err);
-			return res.status(500).json({ message: "Internal Server Error" });
-		});
+  note
+    .save({})
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      console.error("Error creating note:", err);
+      return res.status(500).json({ message: "Internal Server Error" });
+    });
 });
 
-// TODO: Add PUT route to update a note
+// PUT route to update a note
+
+export const updateNote = router.put("/notes", (req, res) => {
+  const { title, content } = req.body;
+  Note.findOneAndUpdate(id, { title: title }, option);
+
+  note
+    .save({})
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      console.error("Error creating note:", err);
+      return res.status(500).json({ message: "Internal Server Error" });
+    });
+});
 
 // Delete a note
 export const deleteNote = router.delete("/notes/:id", async (req, res) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	Note.findByIdAndDelete(id)
-		.then((note) => {
-			if (!note) {
-				return res.status(404).json({ message: "Note not found" });
-			}
-			res.status(200).json({
-				message: `Note title: '${note.title}' deleted successfully`,
-			});
-		})
-		.catch((err) => {
-			console.error("Error deleting note:", err);
-			return res.status(500).json({ message: "Internal Server Error" });
-		});
+  Note.findByIdAndDelete(id)
+    .then((note) => {
+      if (!note) {
+        return res.status(404).json({ message: "Note not found" });
+      }
+      res.status(200).json({
+        message: `Note title: '${note.title}' deleted successfully`,
+      });
+    })
+    .catch((err) => {
+      console.error("Error deleting note:", err);
+      return res.status(500).json({ message: "Internal Server Error" });
+    });
 });
