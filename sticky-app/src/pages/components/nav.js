b076b3c7b4da63bc62b3e98d0535c2588bf8452c
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-08-05 21:48:09 +0100

    .

--- sticky-app/src/pages/components/nav.js
+++ sticky-app/src/pages/components/nav.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
-import './style/style.css'
+
 
 export default function Nav(){
     return(
         <div id='nav'>
-            <div>Cerulean Lion Sticky Note App</div>
+            <div style={{
+                position: "absolute",
+                width: "100%",
+
+
+            }}
+            
+            >Cerulean Lion Sticky Note App</div>
         </div>
     )
 }
