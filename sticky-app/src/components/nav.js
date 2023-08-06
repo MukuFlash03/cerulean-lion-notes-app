bde64c01db0cba7296061c9b516c5a18aa918512
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-08-06 19:14:25 +0100

    styling nav

--- sticky-app/src/components/nav.js
+++ sticky-app/src/components/nav.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 
 export default function Nav(){
     return(
-        <nav>
-            Cerulean Lion Sticky Note App
+        <nav className="absolute w-16 bg-blue-600 flex">
+            <h4>Cerulean Lion Sticky Note App</h4>
         </nav>
     )
 }
