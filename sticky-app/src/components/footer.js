db0fe6da7d090b669d394d649f2cf81409bba0e6
Dedios <dediosprogramming@gmail.com>
2023-08-08 02:18:32 -0400

    styling example

--- sticky-app/src/components/footer.js
+++ sticky-app/src/components/footer.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-
-export default function Footer(){
-    return(
-        <footer>
-            © 2023 Cerulean Lion Sticky Note App
-        </footer>
-    )
+export default function Footer() {
+  return (
+    <div className="bg-sky-400 h-80">
+      <h1 className="text-red-600">© 2023 Cerulean Lion Sticky Note App</h1>
+    </div>
+  );
 }
