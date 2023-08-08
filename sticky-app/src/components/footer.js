336362301185160edbf2eb19e08225578bbd6b06
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-08-08 18:14:36 +0100

    del exta func in footer.js

--- sticky-app/src/components/footer.js
+++ sticky-app/src/components/footer.js
@@ -9,11 +9,5 @@ export default function Footer(){
         </footer>
     )
 }
-export default function Footer() {
-  return (
-    <div className="bg-sky-400 h-80">
-      <h1 className="text-red-600">© 2023 Cerulean Lion Sticky Note App</h1>
-    </div>
-  );
-}
+
 
